028c5cf4d347aebc7505ae0326fc01ea07c7ae45
yuliiakandych <y_kandych@fanshaweonline.ca>
2019-03-18 12:31:08 -0400

    light

--- js/main.js
+++ js/main.js
@@ -5,18 +5,37 @@
 	const 	sigils = document.querySelectorAll('.sigil-container'),
 			lightbox = document.querySelector('.lightbox'),
 			video = document.querySelector('video');
+			lbClose = document.querySelector('.lightbox-close');
 
 	function toggleLightbox() {
+		// debugger;
+		let targetHouse = this.className.split(" ")[1];
+		 //this gives us a lowercase house name -> the secons class 
+		 //on all of the ie stark,  baratheon ..
+
+		 //flip this to uppercase
+		 targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+		 	// targetHouse.split(1));
+
+		 video.src = `video/House-${targetVid}.mp4`; 
 		lightbox.classList.add('lightbox-on');
+
+		video.load();
+		video.play();
 	}
 
 	function closeLightbox() {
 		lightbox.classList.remove('lightbox-on');
+
+		// rewind the current video and pause it
+		video.currentTime = 0;
+		video.pause();
 	}
 
 	sigils.forEach(sigil => sigil.addEventListener('click', toggleLightbox));
 
 	video.addEventListener('ended', closeLightbox);
+	lbClose.addEventListener('click', closeLightbox);
 
 
 })();
