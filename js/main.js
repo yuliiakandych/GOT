27e40e733ded5bd5499c5170ff0eb441080f4895
yuliiakandych <y_kandych@fanshaweonline.ca>
2019-04-12 13:08:04 -0400

    changes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@
 			lightbox = document.querySelector('.lightbox'),
 			video = document.querySelector('video'),
 			lbClose = document.querySelector('.lightbox-close'),
-			topBanners = document.querySelector('#houseImages')
+			topBanners = document.querySelector('#houseImages'),
+			tagline = document.querySelector('.house-name');
+			houseInfo = document.querySelector('.house-info')
+
+const houseData = [
+`House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.
+`];
+
+
+
+
 
 	function toggleLightbox() {
 		// debugger;
@@ -17,6 +27,11 @@
 		 //flip this to uppercase
 		 targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
 		 	// targetHouse.split(1));
+
+		 	tagline.textContent = `House ${targetVid}`;
+
+		 	// populate the house content
+		 	houseInfo.textContent = houseData
 
 		 video.src = `video/House-${targetVid}.mp4`; 
 		lightbox.classList.add('lightbox-on');
@@ -39,7 +54,8 @@
 		// grab the data-offset number from the shield we're clicking on and than do a bit of match 
 		let currentOffset = this.dataset.offset * offSet;
 
-		topBanners.style.right = currentOffset + "px";
+		// topBanners.style.right = currentOffset + "px";
+		TweenMax.to(topBanners, 0.7, {right: currentOffset});
 	}
 
 
